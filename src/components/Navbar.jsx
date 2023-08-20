03fe834af17b4afc6d28219b69c7757a5ac5389d
sbrycbc <sabriyecbc@gmail.com>
2023-08-20 15:50:58 +0200

    added Navbar.jsx...

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center h-20'>
+         <div>
+            <h1>BEACHES!!!!</h1>
+         </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
